bef1ec56a074ef4e41a19a3d9a9a9cabda4306ab
tolchawa <tolchawa@gmail.com>
2023-12-28 14:16:11 +0100

    dev

--- bot.py
+++ bot.py
@@ -1,8 +1,14 @@
 import discord
 from discord.ext import commands
 
+intents = discord.Intents.default()
+# Enable any additional intents as needed
+# For example, if you need member join events:
+# intents.members = True
+
+
 # Create an instance of the bot
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 # Define an event
 @bot.event
@@ -13,6 +19,14 @@ async def on_ready():
 @bot.command()
 async def hello(ctx):
     await ctx.send('Hello World!')
+    
+@bot.event
+async def on_error(event, *args, **kwargs):
+    with open('err.log', 'a') as f:
+        if event == 'on_message':
+            f.write(f'Unhandled message: {args[0]}\n')
+        else:
+            raise
 
 # Run the bot with your token
 bot.run('MTE4OTkxNDIxMTQ4MTAzMDY4Ng.G_OLEn.HeZZrKeD16e4cZh1DIjoB3H_JMYY9WMkZma86M')
